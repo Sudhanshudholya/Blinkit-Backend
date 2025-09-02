49aab5bc6a0f9c0a44d64e6eb18edec1ae59b3fa
sudhanshu dholya <sudhanshudholya069@gmail.com>
2025-09-02 04:17:47 -0700

    Category CRUD Functionality

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,4 +1,6 @@
 import CategoryModel from "../models/category.model.js";
+import ProductModel from "../models/product.model.js";
+import SubCategoryModel from "../models/subCategory.model.js";
 
 //ADD CATEGORY CONTROLLER
 
@@ -39,6 +41,109 @@ export const addCategoryController = async (req, res) => {
       message: error.message || error,
       success: false,
       error: true,
+    });
+  }
+};
+
+//GET CATEGORY CONTROLLER
+
+export const getCategoryController = async (req, res) => {
+  try {
+    const data = await CategoryModel.find().sort({ createdAt: -1 });
+
+    return res.status(200).send({
+      status: true,
+      message: "Category fetched successfully",
+      data: data,
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return res.satus(500).send({
+      status: false,
+      message: error.message || message,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+//UPDATE CATEGORY CONTROLLER
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { _id, name, image } = req?.body;
+
+    const update = await CategoryModel.updateOne(
+      {
+        _id,
+      },
+      {
+        name,
+        image,
+      },
+      { new: true }
+    );
+
+    return res.status(200).send({
+      status: true,
+      message: "Category Updated Successfully",
+      data: update,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+};
+
+//DELETE CATEGORY CONTROLLER
+
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { _id } = req.body;
+
+    const checkSubCategory = await SubCategoryModel.find({
+      category: {
+        $in: [_id],
+      },
+    }).countDocuments();
+
+    const checkProduct = await ProductModel.find({
+      category: {
+        $in: [_id],
+      },
+    }).countDocuments();
+
+    if (checkSubCategory > 0 || checkProduct > 0) {
+      return res.status(400).send({
+        status: false,
+        message: " Category is already used can not be deleted",
+        error: true,
+        success: false,
+      });
+    }
+
+    const deleteCategory = await CategoryModel.deleteOne({ _id: _id });
+
+    return res.status(200).send({
+      status: true,
+      message: "Category deleted successfully",
+      data: deleteCategory,
+      success: true,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      status: false,
+      message: error.message || error,
+      error: true,
+      success: false,
     });
   }
 };
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { addCategoryController } from "../controllers/category.controller.js";
+import { addCategoryController, deleteCategoryController, getCategoryController, updateCategoryController } from "../controllers/category.controller.js";
 import auth from "../middleware/auth.js";
 
 const categoryRouter = Router()
 
 categoryRouter.post('/add-category', auth, addCategoryController)
+categoryRouter.get('/get-category', getCategoryController)
+categoryRouter.put('/update-category', auth, updateCategoryController)
+categoryRouter.delete('/delete-category', auth, deleteCategoryController)
 
 export default categoryRouter
